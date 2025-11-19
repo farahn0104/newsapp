71301a2fd06a5361ce8247e4a28a13cb5c235cd9
Farhan Ansari <mdfarhankic85@gmail.com>
2025-11-19 23:37:48 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/newsapp/"  // YEH LINE ADD KARO - tere repository ke name se
 })
